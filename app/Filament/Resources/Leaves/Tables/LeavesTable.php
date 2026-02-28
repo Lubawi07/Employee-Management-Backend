55eadd2d5c5cbea28f8e14bbbf487947f3ac1c9b
Lubawi <mydeardika@gmail.com>
2026-02-28 16:01:37 +0700

    nambahin fitur export/import

--- app/Filament/Resources/Leaves/Tables/LeavesTable.php
+++ app/Filament/Resources/Leaves/Tables/LeavesTable.php
@@ -2,11 +2,14 @@
 
 namespace App\Filament\Resources\Leaves\Tables;
 
+use App\Filament\Exports\LeavesExporter;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ExportAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class LeavesTable
@@ -14,12 +17,19 @@ class LeavesTable
     public static function configure(Table $table): Table
     {
         return $table
+            ->headerActions([
+                ExportAction::make()
+                    ->exporter(LeavesExporter::class)
+                    ->label('Export Cuti/izin')
+            ])
             ->columns([
                 TextColumn::make('id')
                     ->label('ID')
-                    ->sortable(),
+                    ->sortable()
+                    ->rowIndex(),
                 TextColumn::make('employee.user.name')
-                    ->label('Karyawan'),
+                    ->label('Karyawan')
+                    ->searchable(),
                 TextColumn::make('leave_type')
                     ->label('Tipe Cuti')
                     ->badge(),
@@ -37,13 +47,28 @@ class LeavesTable
                     }),
                 TextColumn::make('user.name')
                     ->label('Disetujui oleh'),
-                // TextColumn::make('reason')
-                //     ->label('Alasan'),
+                TextColumn::make('reason')
+                    ->label('Alasan')
+                    ->toggleable(true),
                 TextColumn::make('created_at')
                     ->label('Dibuat')
             ])
             ->filters([
-                //
+                SelectFilter::make('leave_type')
+                    ->label('Tipe Cuti')
+                    ->multiple()
+                    ->options([
+                        'Sakit' => 'SICK',
+                        'Izin' => 'PERMISSIONS',
+                        'Penting' => 'EMERGANCY'
+                    ]),
+                SelectFilter::make('persetujuan')
+                    ->label('Status')
+                    ->multiple()
+                    ->options([
+                        'Disetujui' => 'APPROVED',
+                        'Ditolak' => 'REJECTED'
+                    ])
             ])
             ->recordActions([
                 DeleteAction::make(),
